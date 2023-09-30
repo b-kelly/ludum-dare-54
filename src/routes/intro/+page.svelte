ab25a87fc5ac68ca388a76a47829afe49c71bf61
Ben Kelly <b-kelly@users.noreply.github.com>
2023-09-30 00:08:00 -0400

    drop bad session creation code

--- src/routes/intro/+page.svelte
+++ src/routes/intro/+page.svelte
@@ -1,16 +1,3 @@
-<script lang="ts">
-	import { gameState } from '$lib/gameState';
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		// set the userid to a new value
-		gameState.update((gs) => {
-			gs.userId = 'todo_userid_new';
-			return gs;
-		});
-	});
-</script>
-
 <h1 class="bm-page-title">Welcome to your museum!</h1>
 <p>
 	You might notice it's looking pretty empty, but that's okay, it's your job to fill it with new art
